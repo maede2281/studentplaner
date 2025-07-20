3cbadc54fc78c2667be3e2e7426b4036275d1dc6
Maede Borzoo <maedborzooi@gmail.com>
2025-07-20 19:54:00 +0430

    aaa

---
+++ script.js
@@ -0,0 +1,12 @@
+function addtask(){
+    const input=document.getElementById('taskinpute');
+    const task=input.ariaValueMax.trim();
+
+    if(task){
+        const list = document.getElementById('tasklist');
+        const li = document.createElement('li');
+        li.textContent=task;
+        list.appendChild(li);
+        input.value='';
+    }
+}
